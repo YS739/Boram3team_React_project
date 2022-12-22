a21ff47438d3ac0c916a4904703a933a26797b27
heeryoung <winterbooks@naver.com>
2022-12-22 22:50:20 +0900

    Add 회원가입 기능 구현 성공 아직 유효성 검증 등 추가 구현 필요함

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import useInput from "../../hooks/useInput";
+import { __signUp } from "../../redux/modules/usersSlice";
 
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  
+  const dispatch = useDispatch();
+
+  const { error } = useSelector((state) => state.users);
+
+  const [ID, setID, onChangeIDHandler] = useInput();
+  const [PW, setPW, onChangePWHandler] = useInput();
+
+  const newUser = {
+    ID,
+    PW,
+    user: true,
+  };
+
+  const addHandler = (e) => {
+    e.preventDefault();
+    dispatch(__signUp(newUser));
+    alert("가입완료");
+
+    setID('');
+    setPW('');
+  };
+
+  if (error) {
+        return <div>{error.massage}</div>
+  }
+
+  return (
+    <div>
+      <h3>회원가입</h3>
+      <form>
+        <span>아이디</span>
+        <input type="text" id="ID" name="ID" method="post"
+        vlaue={ID} onChange={onChangeIDHandler}/>
+        <span>비밀번호</span>
+        <input type="text" id="PW" name="PW" method="post"
+        vlaue={PW} onChange={onChangePWHandler}/>
+        <button onClick={addHandler}>가입하기</button>
+      </form>
+    </div>
+  )
 };
 
 export default SignUpPage;
--- src/redux/modules/usersSlice.js
+++ src/redux/modules/usersSlice.js
@@ -1,11 +1,50 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = {};
+// Initial State
+const initialState = {
+  users: [],
+  isLoading: false,
+  error: null,
+};
+
+// Thunk 함수
+export const __signUp = createAsyncThunk(
+  // 첫 번째 인자: action value
+  "users/signUp",
+  // 두 번째 인자: 콜백함수
+  async (payload, thunkAPI) => {
+    try {
+      await axios.post("http://localhost:3001/users", payload);
+      const data = await axios.get("http://localhost:3001/users");
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
 const usersSlice = createSlice({
-  name: "usersSlice",
+  name: "users",
   initialState,
   reducers: {},
+  extraReducers: {
+    [__signUp.pending]: (state) => {
+      state.isLoading = true;
+      console.log("pengding")
+    },
+    [__signUp.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.users = action.payload;
+      console.log("fulfilled")
+      console.log("state.users:", state.users)
+    },
+    [__signUp.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    }
+    
+  }
 });
 
-export default usersSlice;
+export default usersSlice.reducer;
