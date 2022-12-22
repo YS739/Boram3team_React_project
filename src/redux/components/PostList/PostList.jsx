123ef823e372703653c0aa5cc57a623fffa98383
hwan910 <psh5575@gmail.com>
2022-12-22 23:59:24 +0900

    Add- thunk 와 axios 를 사용하여 홈페이지에 데이터 불러오기 성공

--- src/redux/components/PostList/PostList.jsx
+++ src/redux/components/PostList/PostList.jsx
@@ -1,19 +1,29 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { __getPosts } from "../../modules/postsSlice";
 
-const getPosts = () => {
-  return axios.get("http://localhost:3001/posts");
-};
 const PostList = () => {
-  const posts = useQuery(["posts"], getPosts, {
-    onSuccess: (data) => {
-      console.log("성공", data.data);
-    },
-  });
+  const dispatch = useDispatch();
+  const { isLoading, error, posts } = useSelector((state) => state.postsSlice);
+
+  useEffect(() => {
+    dispatch(__getPosts());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>로딩 중... </div>;
+  }
+  if (error) {
+    return <div>{error.message}</div>;
+  }
 
   return (
     <>
-      <div>안녕안녕 핑크퐁!</div>
+      <div>
+        {posts.map((post) => {
+          return <div key={post.id}>{post.title}</div>;
+        })}
+      </div>
     </>
   );
 };
