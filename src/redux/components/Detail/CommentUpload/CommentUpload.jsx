fc85334448f183cd434f2836b60d3a54d1837806
YS739 <suk921@gmail.com>
2022-12-22 23:27:25 +0900

    Add thunk api 댓글 등록 페이지 및 slice 파일, thunk 함수 구현

--- src/redux/components/Detail/CommentUpload/CommentUpload.jsx
+++ src/redux/components/Detail/CommentUpload/CommentUpload.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const CommentUpload = () => {
-  return <div></div>;
+  const [comment, setComment] = useState();
+  //  TODO: dispatch import 추가
+
+  const onChangeCommentHandler = (e) => {
+    setComment(e.target.value);
+  };
+
+  const onSubmitCommentHandler = (e) => {
+    if (comment) {
+      e.preventDefault();
+      const newComment = {
+        id: uuidv4(),
+        comment,
+      };
+      // TODO: dispatch, input 초기화 추가
+    } else if (!comment) {
+      e.preventDefault();
+      document.getElementById("comment").focus();
+      alert("댓글을 입력해주세요.");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitCommentHandler}>
+        <input
+          id="comment"
+          value={comment}
+          maxLength="30"
+          placeholder="댓글을 입력해주세요."
+          onChange={onChangeCommentHandler}
+        ></input>
+        <button>댓글 등록</button>
+      </form>
+    </div>
+  );
 };
 
 export default CommentUpload;
--- src/redux/modules/commentsSlice.js
+++ src/redux/modules/commentsSlice.js
@@ -1,6 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {};
+
+export const __postComment = createAsyncThunk(
+  "comments/postComment",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.post("http://localhost:3001/comments", payload);
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
 const commentsSlice = createSlice({
   name: "commentsSlice",
