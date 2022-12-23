b53ccf71dde2696fd9b2c7d1f86ae6a8ac82ac59
YS739 <suk921@gmail.com>
2022-12-23 12:04:23 +0900

    Add thunk 함수

--- src/redux/modules/postsSlice.js
+++ src/redux/modules/postsSlice.js
@@ -1,11 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {};
 
+export const __editPost = createAsyncThunk(
+  "post/editPost",
+  async (payload, thunkAPI) => {
+    try {
+      await axios.post(`http://localhost:3001/post`, payload);
+      return thunkAPI.fulfillWithValue(payload);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 const postsSlice = createSlice({
-  name: "postsSlice",
+  name: "posts",
   initialState,
   reducers: {},
+  extraReducers: {
+    [__editPost.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__editPost.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.post = [...state.post, action.payload];
+    },
+    [__editPost.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.post = action.payload;
+    },
+  },
 });
 
-export default postsSlice;
+export default postsSlice.reducer;
