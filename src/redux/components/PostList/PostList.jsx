4c3b16f33e6931b636cf09b25306b4c4556f3cc4
hwan910 <psh5575@gmail.com>
2022-12-23 13:57:13 +0900

    Add 본문조회 필수기능 구현 완료

--- src/redux/components/PostList/PostList.jsx
+++ src/redux/components/PostList/PostList.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { __getPosts } from "../../modules/postsSlice";
+import { Article, H1, PostBox, PostLike, Section } from "./style";
 
 const PostList = () => {
   const dispatch = useDispatch();
   const { isLoading, error, posts } = useSelector((state) => state.postsSlice);
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(__getPosts());
@@ -19,11 +22,31 @@ const PostList = () => {
 
   return (
     <>
-      <div>
+      <Section>
+        <H1>토론주제</H1>
+
         {posts.map((post) => {
-          return <div key={post.id}>{post.title}</div>;
+          return (
+            <Article
+              key={post.id}
+              onClick={() => {
+                navigate(`/${post.id}`);
+              }}
+            >
+              <PostBox>
+                <div>논제: {post.title}</div>
+                <div>
+                  <div>
+                    A: {post.categoryA} vs B: {post.categoryB}
+                  </div>
+                  <div></div>
+                </div>
+              </PostBox>
+              <PostLike>👍: {post.like}</PostLike>
+            </Article>
+          );
         })}
-      </div>
+      </Section>
     </>
   );
 };
