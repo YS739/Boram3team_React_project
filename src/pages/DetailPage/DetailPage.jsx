b694cbafdc2afbb97668cf2a4916006e3c293f3c
mr-chacha <hoitchac@gmail.com>
2022-12-23 09:21:09 +0900

    Test 깃연결 테스트중

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const DetailPage=()=> {
-  return <div>DetailPage</div>;
-}
+const DetailPage = () => {
+  return <div>디테일 포스트 테스트</div>;
+};
 
 export default DetailPage;
