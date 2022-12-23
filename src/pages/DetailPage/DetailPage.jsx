f3005eebc425e179562da9ce6b92f4cf4771e196
mr-chacha <hoitchac@gmail.com>
2022-12-23 10:21:43 +0900

    Implement 본문조회성공

--- src/pages/DetailPage/DetailPage.jsx
+++ src/pages/DetailPage/DetailPage.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 const DetailPage = () => {
-  return <div>디테일 포스트 테스트</div>;
+  const [posts, setPosts] = useState();
+
+  const fetchTodos = async () => {
+    const { data } = await axios.get("http://localhost:3005/post");
+    setPosts(data); // 서버로부터 fetching한 데이터를 useState의 state로 set 합니다.
+  };
+
+  useEffect(() => {
+    // effect 구문에 생성한 함수를 넣어 실행합니다.
+    fetchTodos();
+  }, []);
+
+  return (
+    <div>
+      {posts?.map((post) => {
+        return (
+          <div key={post.id}>
+            <h1>토론주제 :{post.title}</h1>
+            <h2>선택분류</h2>
+            <p>A : {post.aContent}</p>
+            <br></br>
+            <p>B : {post.bContent}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default DetailPage;
