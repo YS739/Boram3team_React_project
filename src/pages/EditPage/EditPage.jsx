8eefd00bbae6f4ab2ecfd5171be68557b2167f51
YS739 <suk921@gmail.com>
2022-12-23 12:46:35 +0900

    Edit 본문 수정 페이지 __getPost 구현되면 마저 수정하기

--- src/pages/EditPage/EditPage.jsx
+++ src/pages/EditPage/EditPage.jsx
@@ -1,31 +1,44 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router";
 import { v4 as uuidv4 } from "uuid";
 import { __editPost } from "../../redux/modules/postsSlice";
 
 const EditPage = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   // TODO: 새로고침 오류 방지 - useEffect, __getPost 쓰기
   // useEffect(()=>{
-  //   dispatch()
-  // })
+  //   dispatch(__getPost())
+  // },[dispatch]);
+
+  // FIXME: 본문 등록 페이지가 reducer 등등으로 연결이 안 되어 있어서 state가 빈 배열이 나옴
+  // __getPost함수를 만들어 useEffect를 써야함
+  const globalPost = useSelector((state) => state.posts);
+  console.log(globalPost);
+
+  const param = useParams();
+  const { error } = useSelector((state) => state.posts);
+
+  const thePost = globalPost.find((post) => post.id === param.id);
+  const navigate = useNavigate(`/${thePost.id}`);
 
   // TODO: useInput custom hook 쓰기
   const [title, setTitle] = useState();
   const [aContents, setAContents] = useState();
   const [bContents, setBContents] = useState();
 
+  if (error) {
+    return <div>{error.massage}</div>;
+  }
+
   const editPostHandler = (e) => {
     e.preventDefault();
-
     if (title && aContents && bContents) {
       if (window.confirm("수정하시겠습니까?") === true) {
-        navigate("/upload");
+        navigate();
         const editPost = {
-          id: uuidv4(), //TODO: comment.id 등으로 바꾸기
+          id: thePost.id,
           title,
           aContents,
           bContents,
@@ -84,7 +97,7 @@ const EditPage = () => {
               value={bContents}
               onChange={bContentsChangeHandler}
             />
-            <button>수정하기</button>
+            <button>수정 완료</button>
           </section>
         </form>
       </div>
