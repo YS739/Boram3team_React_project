35cca934a6fba8d2db979f0febe83565dbc1e1d4
YS739 <suk921@gmail.com>
2022-12-23 20:49:03 +0900

    Edit 본문 수정 페이지 getPost로 받아오는 부분 제외 구현 완료

--- src/pages/EditPage/EditPage.jsx
+++ src/pages/EditPage/EditPage.jsx
@@ -14,50 +14,60 @@ const EditPage = () => {
 
   // FIXME: 본문 등록 페이지가 reducer 등등으로 연결이 안 되어 있어서 state가 빈 배열이 나옴
   // __getPost함수를 만들어 useEffect를 써야함
-  const globalPost = useSelector((state) => state.posts);
-  console.log(globalPost);
+  // const globalPost = useSelector((state) => state.posts);
+  // console.log(globalPost);
 
-  const param = useParams();
-  const { error } = useSelector((state) => state.posts);
+  // const param = useParams();
+  // const { error } = useSelector((state) => state.posts);
 
-  const thePost = globalPost.find((post) => post.id === param.id);
-  const navigate = useNavigate(`/${thePost.id}`);
+  // const thePost = globalPost.find((post) => post.id === param.id);
+  // const navigate = useNavigate(`/${thePost.id}`);
 
   // TODO: useInput custom hook 쓰기
   const [title, setTitle] = useState();
   const [aContents, setAContents] = useState();
   const [bContents, setBContents] = useState();
 
-  if (error) {
-    return <div>{error.massage}</div>;
-  }
+  // if (error) {
+  //   return <div>{error.massage}</div>;
+  // }
 
   const editPostHandler = (e) => {
     e.preventDefault();
     if (title && aContents && bContents) {
+      e.preventDefault();
+
       if (window.confirm("수정하시겠습니까?") === true) {
-        navigate();
+        // navigate();
         const editPost = {
-          id: thePost.id,
+          id: uuidv4(), // thePost.id,
           title,
           aContents,
           bContents,
         };
         dispatch(__editPost(editPost));
       }
+      // input 초기화
+      setTitle("");
+      setAContents("");
+      setBContents("");
     }
 
     if (!title) {
+      // e.preventDefault();
       document.getElementById("title").focus();
       alert("주제를 입력해주세요");
       return;
     }
     if (!aContents) {
+      // e.preventDefault();
       document.getElementById("aContents").focus();
       alert("A의 내용을 입력해 주세요");
       return;
     }
     if (!bContents) {
+      // e.preventDefault();
+
       document.getElementById("bContents").focus();
       alert("B의 내용을 입력해 주세요");
       return;
--- src/redux/modules/postsSlice.js
+++ src/redux/modules/postsSlice.js
@@ -1,14 +1,19 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const initialState = {};
+const initialState = {
+  post: [],
+  isLoading: false,
+  error: null,
+};
 
 export const __editPost = createAsyncThunk(
   "post/editPost",
   async (payload, thunkAPI) => {
     try {
       await axios.post(`http://localhost:3001/post`, payload);
-      return thunkAPI.fulfillWithValue(payload);
+      const data = await axios.get(`http://localhost:3001/post`);
+      return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
       return thunkAPI.rejectWithValue(error);
     }
@@ -25,7 +30,7 @@ const postsSlice = createSlice({
     },
     [__editPost.fulfilled]: (state, action) => {
       state.isLoading = false;
-      state.post = [...state.post, action.payload];
+      state.post = action.payload;
     },
     [__editPost.rejected]: (state, action) => {
       state.isLoading = false;
