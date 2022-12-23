83f544661b5bfaadcb056d9c6f7c563f103896b5
YS739 <suk921@gmail.com>
2022-12-23 20:16:00 +0900

    Fix 댓글 등록 안 되는 거 수정 style 컴포넌트 싹 빼니 해결 됨

--- src/redux/components/Detail/Comments/Comments.jsx
+++ src/redux/components/Detail/Comments/Comments.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { CommentBody, CommentUser } from "./style";
 
 const Comments = () => {
   const { error } = useSelector((state) => state.comments);
-  const globalComment = useSelector((state) => state.comments.comment);
+  const { comment } = useSelector((state) => state.comments);
 
   if (error) {
     // 데이터를 불러오다가 오류가 나면 화면에 오류 메시지 표시
@@ -14,17 +13,15 @@ const Comments = () => {
   return (
     <div>
       {/* optional chaining('?') 사용 - 새로고침했을 때 오류 해결*/}
-      {globalComment?.map((co) => {
+      {comment?.map((co) => {
         return (
-          <>
-            <CommentBody>
-              <div>카테고리 내용</div>
-              <CommentUser>
-                <span key={co.id}>{co.comment}</span>
-                <span>작성자</span>
-              </CommentUser>
-            </CommentBody>
-          </>
+          <div key={co.id}>
+            <div>카테고리 내용</div>
+            <div>
+              <span>{co.comment}</span>
+              <span>작성자</span>
+            </div>
+          </div>
         );
         // TODO: 작성자, 선택한 카테고리 내용 추가
       })}
