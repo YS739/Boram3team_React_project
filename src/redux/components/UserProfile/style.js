e43293f7327beaae5487f1479d5a279bd0ef572d
hwan910 <psh5575@gmail.com>
2022-12-24 20:58:18 +0900

    Implement 내가 등록한 토론 불러오기 기능 구현

---
+++ src/redux/components/UserProfile/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  border: 1px solid red;
+`;
+export { Section };
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import commentsSlice from "../modules/commentsSlice";
-import postsSlice from "../modules/postsSlice";
+import comments from "../modules/commentsSlice";
+import posts from "../modules/postsSlice";
 import users from "../modules/usersSlice";
 
 const store = configureStore({
   reducer: {
-    commentsSlice: commentsSlice,
-    postsSlice: postsSlice,
-    users,
+    comments: comments,
+    posts: posts,
+    users: users,
   },
 });
 
