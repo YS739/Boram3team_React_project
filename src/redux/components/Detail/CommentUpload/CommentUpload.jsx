6db45278a7a492119cfe59d2cb2f46b75d8c3534
YS739 <suk921@gmail.com>
2022-12-26 23:04:37 +0900

    Implement 댓글 한 번만 달기 기능 구현 완료

--- src/redux/components/Detail/CommentUpload/CommentUpload.jsx
+++ src/redux/components/Detail/CommentUpload/CommentUpload.jsx
@@ -15,51 +15,76 @@ import CustomButtons from "../../CustomButtons";
 import useInput from "../../../../hooks/useInput";
 
 const CommentUpload = () => {
+  const dispatch = useDispatch();
   const param = useParams();
+  const { comments } = useSelector((state) => state.comments);
   const { posts } = useSelector((state) => state.posts);
   const thePost = posts.find((post) => post.id === param.id);
   const theA = thePost?.categoryA;
   const theB = thePost?.categoryB;
   const theId = thePost?.id;
   const currentUserDi = localStorage.getItem("id");
-  const dispatch = useDispatch();
+
   //useInput
   const [comment, setComment, onChangeCommentHandler] = useInput();
+
+  //현재 포스트의 댓글들
+  const thePostComment = comments?.filter(
+    (theCo) => theCo.postNumber === param.id
+  );
+  console.log(thePostComment);
+
+  //현재 포스트의 user 댓글
+  const userComment = thePostComment?.find(
+    (theCo) => theCo.user === currentUserDi
+  );
+  console.log(userComment);
+
+  // 댓글 등록 버튼
   const onSubmitCommentHandler = (e) => {
-    // A,B 중 선택한 값 찾기 - 해당 value가 isA에 들어감
+    e.preventDefault();
+    // category A,B 중 선택한 값 찾기 - 해당 value = isA
     const categories = document.getElementsByName("category");
     const selected = Array.from(categories).find((choice) => choice.checked);
 
-    // 댓글 입력하고 카테고리 선택했을 때
-    if (comment && selected) {
-      e.preventDefault();
-      const newComment = {
-        id: uuidv4(),
-        postNumber: theId,
-        user: currentUserDi,
-        comment,
-        isA: selected.value,
-        date: new Date(),
-      };
-      dispatch(__postComment(newComment));
+    // 현재 user 댓글이 있을 때
+    if (userComment) {
+      alert("댓글은 한 번만 등록할 수 있습니다.");
+    }
 
-      // input 초기화
-      setComment("");
-      // category 선택 초기화
-      categories.forEach((category) => {
-        category.checked = false;
-      });
-    }
-    // 댓글을 작성하지 않았을 때 alert
-    else if (!comment) {
-      e.preventDefault();
-      document.getElementById("comment").focus();
-      alert("댓글을 입력해주세요.");
-    }
-    // 카테고리를 선택하지 않았을 때 alert
-    else if (!selected) {
-      e.preventDefault();
-      alert("카테고리를 선택해주세요.");
+    // 현재 user 댓글이 없을때
+    else if (userComment === undefined) {
+      // 댓글을 작성하지 않았을 때
+      if (!comment) {
+        document.getElementById("comment").focus();
+        alert("댓글을 입력해주세요.");
+      }
+
+      // 카테고리를 선택하지 않았을 때
+      else if (!selected) {
+        alert("카테고리를 선택해주세요.");
+      }
+
+      // 댓글 입력, 카테고리 선택했을 때
+      else if (comment && selected) {
+        e.preventDefault();
+        const newComment = {
+          id: uuidv4(),
+          postNumber: theId,
+          user: currentUserDi,
+          comment,
+          isA: selected.value,
+          date: new Date(),
+        };
+        dispatch(__postComment(newComment));
+
+        // input 초기화
+        setComment("");
+        // category 선택 초기화
+        categories.forEach((category) => {
+          category.checked = false;
+        });
+      }
     }
   };
 
