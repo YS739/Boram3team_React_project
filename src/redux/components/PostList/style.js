3522ed82dd8a3bc8aa2d1ced3d5e13ecc37e3dd6
hwan910 <psh5575@gmail.com>
2022-12-27 19:14:03 +0900

    Implement 메인페이지 CSS 완료

--- src/redux/components/PostList/style.js
+++ src/redux/components/PostList/style.js
@@ -1,35 +1,84 @@
 import styled from "styled-components";
 
-const Section = styled.section``;
+const Section = styled.section`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: -30px;
+`;
 
 const H1 = styled.h1`
+  margin-bottom: 15px;
   margin-left: 10px;
   font-size: 30px;
+  padding-left: 10px;
+  margin-left: 180px;
+  border-bottom: 1px solid gray;
+  width: 60%;
 `;
 
 const Article = styled.article`
-  border: 5px solid black;
+  border-radius: 10px;
   margin: 40px;
   font-size: 30px;
+  width: 70%;
+  background-color: ${(props) => props.color};
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 5px 15px;
+  padding: 10px;
+  :hover {
+    transform: scale(1.02);
+    border-radius: 10px;
+  }
 `;
 
 const PostContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  height: 100%;
 `;
 
 const PostBox = styled.div`
-  border: 1px solid gray;
-  width: 85%;
-  margin-right: 10px;
+  width: 100%;
   padding: 10px;
-  :hover {
-    background-color: lightblue;
-  }
+  cursor: pointer;
+`;
+
+const PostTitle = styled.div`
+  display: flex;
+  /* text-align: center; */
+  justify-content: center;
+  align-items: center;
+  height: 150px;
+  padding: 0 30px 30px 50px;
+
+  color: ${(props) => props.font};
+`;
+const CategoryBox = styled.div`
+  padding: 40px 20px 40px 20px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 5px 15px;
+`;
+const PostCategory = styled.div`
+  display: flex;
+  text-align: center;
+`;
+const CategoryContent = styled.div`
+  width: 50%;
+  text-align: center;
+  border-right: ${(props) => props.br};
+  font-weight: bold;
+`;
+const LikeDiv = styled.button`
+  border: none;
+  background-color: transparent;
+  position: relative;
+  right: -90%;
+  text-shadow: -1px 0 #000, 0 1px #000, 1px 0 #000, 0 -1px #000;
 `;
 const PostLike = styled.div`
-  font-size: 50px;
-  color: red;
+  font-size: 40px;
   display: ${(props) => props.dp};
 `;
 
@@ -45,6 +94,8 @@ const BarA = styled.div`
   height: 100%;
   background-color: ${(props) => props.color};
   text-align: center;
+  color: white;
+  font-weight: bold;
 `;
 
 export {
@@ -56,4 +107,9 @@ export {
   PostContainer,
   GageBar,
   BarA,
+  LikeDiv,
+  PostTitle,
+  PostCategory,
+  CategoryContent,
+  CategoryBox,
 };
